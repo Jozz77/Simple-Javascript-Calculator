0aaa0c207ee203aa455b552e36901e31a94a18e9
Jo_Na_Than <mmadujonathan@gmail.com>
2022-06-22 23:45:20 +0100

    Updated a message to my Mentors and the README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Simple Javascript Calculator
  This is a basic arithmetic calculator without a frontend
+This is the link to the Github page: https://jozz77.github.io/Simple-Javascript-Calculator/
--- scripts.js
+++ scripts.js
@@ -1,3 +1,6 @@
+let myMentorMessage = 'Hi Mentors, I am very sorry to share the link to my Github page instead of the link to my repository. Here is the repository link: https://github.com/Jozz77/Simple-Javascript-Calculator.  Thanks for understanding. Click Ok to view the assignment';
+
+alert(myMentorMessage)
 var opr = prompt("Choose your arithmetic symbol you want to perform the calculation on (either +,-,*,/): ")
 var num1 = parseInt(prompt("Enter the first number: "))
 var num2 = parseInt(prompt("Enter the second number:"))
